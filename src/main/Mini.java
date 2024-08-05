6de9572d54807d95bf8461980e86245f0f962dce
seongmin <sungmin990923@naver.com>
2024-08-05 12:25:44 +0900

    최종 수정

--- src/main/Mini.java
+++ src/main/Mini.java
@@ -20,9 +20,7 @@ public class Mini {
 		 
 		OrderStart oS = new OrderStart();
 		OrderHistory oH = new OrderHistory();
-		OrderByCustomer oBC = new OrderByCustomer();
-		SpecialHisory sH = new SpecialHisory();
-		
+	
 		Scanner scanner = new Scanner(System.in);
 		
 		FileWriter fw = new FileWriter("order.txt");
@@ -49,11 +47,11 @@ public class Mini {
 				continue;
 			}
 			else if (num == 3) {
-				oBC.orderByCustomer(scanner);
+				oH.orderByCustomer(scanner);
 				continue;
 			} 
 			else if (num == 4) {
-				sH.specialHistory(scanner);
+				oH.specialHistory(scanner);
 				continue;
 			}
 			else if (num == 5) {
@@ -69,6 +67,7 @@ public class Mini {
 	}
 }
 
+// 상품 클래스
 class Order {
 	
 	String name;
@@ -79,6 +78,7 @@ class Order {
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	String formatDate = curDateTime.format(formatter);
 
+	// 생성자
 	public Order(String name, String productName, int quantity, int price, String formatDate) {
 		super();
 		this.name = name;
@@ -87,17 +87,18 @@ class Order {
 		this.price = price;
 	}
 
-	
-	
 	@Override
 	public String toString() {
 		return "Order [name=" + name + ", productName=" + productName + ", quantity=" + quantity + ", price=" + price
 				+ ", formatDate=" + formatDate + "]";
 	}
-	
+
 }
 
+// 상문 주문 클래스
 class OrderStart {
+	
+	// 상품 주문하는 함수
 	public static void nextOrder(PrintWriter pw ,Scanner scanner, ArrayList<Order> list) {
 
 		String name;
@@ -138,13 +139,12 @@ class OrderStart {
 			if (scanner.hasNextInt()) {
 				quantity = scanner.nextInt();
 
-				if (quantity <= 0) {
-					System.out.println("수량을 제대로 입력해주세요.");
-					scanner.next();
-				} else {
+				if (quantity > 0) {
 					break;
+				} else {
+					System.out.println("수량을 제대로 입력해주세요.");
 				}
-			}else {
+			} else {
 				System.out.println("수량은 숫자를 입력해주세요.");
 				scanner.next();
 			}
@@ -182,8 +182,10 @@ class OrderStart {
 	}
 }
 
+// History 클래스
 class OrderHistory {
 	
+	// 전체 주문 이력 함수
 	public static void orderHistory() throws IOException {
 		FileReader fr = new FileReader("order.txt");
 		BufferedReader br = new BufferedReader(fr);
@@ -199,61 +201,58 @@ class OrderHistory {
 		System.out.println("=========================================================================");
 
 	}
-}
-
-class OrderByCustomer {
 	
+	// 고객별 주문 이력 함수
 	public static void orderByCustomer(Scanner scanner) throws IOException {
-		FileReader fr = new FileReader("order.txt");
-		BufferedReader br = new BufferedReader(fr);
-		
-		int totalPrice = 0;
-		int totalQuantity = 0;
-		
-		String name;
-		
-		scanner.nextLine();
-		System.out.println("고객명 : ");
-		name = scanner.nextLine();
-		
-		System.out.println("==================================고객별 주문이력==================================");
-		while (true) {
-			String str = br.readLine();
-			if (str == null) {
-				break;
-			}
+			FileReader fr = new FileReader("order.txt");
+			BufferedReader br = new BufferedReader(fr);
 			
-			String nameArr[] = str.split(",");
+			int totalPrice = 0;
+			int totalQuantity = 0;
 			
-			String nameArr2[] = nameArr[1].split(":");
+			String name;
 			
-			String quanArr[] =  nameArr[3].split(":");
+			scanner.nextLine();
+			System.out.println("고객명 : ");
+			name = scanner.nextLine();
 			
-			String priceArr[] = nameArr[4].split(":");
+			System.out.println("==================================고객별 주문이력==================================");
 			
-			
+			while (true) {
+				String str = br.readLine();
+				if (str == null) {
+					break;
+				}
+				
+				String nameArr[] = str.split(",");
+				
+				String nameArr2[] = nameArr[1].split(":");
+				
+				String quanArr[] =  nameArr[3].split(":");
+				
+				String priceArr[] = nameArr[4].split(":");
+				
+				
 				if (nameArr2[1].trim().equals(name)) {
 					totalPrice +=  Integer.parseInt(quanArr[1].trim()) * Integer.parseInt(priceArr[1].trim());	// 전체 주문 금액
 					totalQuantity++;
+					}
 				}
-			}
-
-		if (totalQuantity > 0) {
-			
-		System.out.print("전체 주문 건수 :");
-		System.out.println(totalQuantity);
-		
-		System.out.print("전체 주문 금액 :");
-		System.out.println(totalPrice);
-		}else {
-			System.out.println("해당 고객의 주문이 없습니다.");
-		}
-		System.out.println("=============================================================================");
-	}
-}
-
-class SpecialHisory {
 	
+			if (totalQuantity > 0) {			
+				System.out.print("전체 주문 건수 :");
+				System.out.println(totalQuantity);
+				
+				System.out.print("전체 주문 금액 :");
+				System.out.println(totalPrice);
+			}else {
+				System.out.println("해당 고객의 주문이 없습니다.");
+			}
+			System.out.println("=============================================================================");
+		}
+	
+
+		// 특정날짜에 들어온 주문이력 함수
 	public static void specialHistory(Scanner scanner) throws IOException {
 		FileReader fr = new FileReader("order.txt");
 		BufferedReader br = new BufferedReader(fr);
@@ -271,7 +270,7 @@ class SpecialHisory {
 			
 			String dateArr[] = str.split(",");
 			String dateArr2[] = dateArr[5].split(":");
-
+	
 			if (dateArr2[1].trim().contains(SearchDate)) {
 				System.out.println(str);
 			} else {
@@ -281,5 +280,6 @@ class SpecialHisory {
 		System.out.println("=============================================================================");
 		
 	}
-	
+
 }
+
